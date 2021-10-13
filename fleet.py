ba31cab1b4591f5511e25fa871880228cda4ec2e
Brycen Harmon <brycenharmon@gmail.com>
2021-10-13 11:48:48 -0500

    Further devloped the create_fleet() and create_herd() functions by adding the capability of creating hard coded robots and dinosaurs

--- fleet.py
+++ fleet.py
@@ -1,6 +1,12 @@
+from robot import Robot
+
+
 class Fleet:
     def __init__(self):
         self.robots = []
 
     def create_fleet(self):
-        pass
+        robot_1 = Robot("Rob")
+        robot_2 = Robot("Bzzz")
+        robot_3 = Robot("4-OH-4")
+        self.robots.extend([robot_1, robot_2, robot_3])
--- herd.py
+++ herd.py
@@ -6,4 +6,7 @@ class Herd:
         self.dinosaurs = []
 
     def create_herd(self):
-        pass
+        dino_1 = Dinosaur("T-Rex", 4)
+        dino_2 = Dinosaur("Velociraptor", 6)
+        dino_3 = Dinosaur("Triceritops", 5)
+        self.dinosaurs.extend([dino_1, dino_2, dino_3])
