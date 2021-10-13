b9b0105ea86864e611637f55fbc06064b8fda27d
Brycen Harmon <brycenharmon@gmail.com>
2021-10-13 10:28:14 -0500

    added all necessary classes, and filled in information from UML.

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = ""
+        self.health = 100
+        self.attack_power = 1
+
+    def attack(self, robot):
+        pass
